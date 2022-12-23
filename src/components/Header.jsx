4551f2129de98534cc3697cc1f183ca78afcac5c
Bioboy <biosabogdan@gmail.com>
2022-12-23 19:32:14 +0200

    created the spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
   }, [auth]);
 
   return (
-    <div className="bg-white border-b shadow-lg sticky top-0 z-50">
+    <div className="bg-white border-b shadow-lg sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div
           className="flex items-center text-lg cursor-pointer font-bold"
