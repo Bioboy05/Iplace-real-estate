5d5c27b3016df5a273f9c28326716285825689c5
Bioboy <biosabogdan@gmail.com>
2023-01-03 20:50:57 +0200

    Completed the Offers Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,233 @@
-import React from "react";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 import Slider from "../components/Slider";
+import { db } from "../firebase";
 
 export default function Home() {
+  //Offers
+  const [offerListings, setOfferListings] = useState(null);
+  //now we fill this offerListing using useEffect
+  useEffect(() => {
+    async function fetchListings() {
+      //we use try and catch, in case we have an error
+      try {
+        //for the try we have to get the address reference
+        //we create a const that uses collection method from firestore, that takes the db (from firebase file), and the collection we want to target is "listings"
+        const listingsRef = collection(db, "listings");
+        //create the query which is the condition or the limit of that request
+        //q is using the query method from firestore
+        //then we get the listingsRef, then we want to say where we get the information, so we use "where" from firestore, and the place we want to get is:
+        //where the offer is equal to true(if the offer is true we're going to fetch them)
+        //after that we use the orderBy method from firestore, and we're going to order it by timestamp, in a descending way(the one is created last is going to be the first in the list)
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(4) //limiting the listing only of the 4 listings in db
+        );
+        //we need to get the information using snapshot
+        //execute the query
+        //so we create a querySnap which is going to wait for getDocs from firestore
+        const querySnap = await getDocs(q); //and we just put the query inside
+        //this is going to get the information and save it inside a list, then set it inside offerListing from useState
+        const listings = [];
+        //use forEach to loop thorough the querySnap
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+          //now we have everything inside this listings
+        });
+        //we can just set the offer listings to listings
+        setOfferListings(listings);
+      } catch (error) {
+        //avoid toasting the error here, because it would be annoying for the users/ we just log it in for us
+        console.log(error);
+      }
+    }
+    fetchListings();
+  }, []);
+
+  //Places for Rent
+  const [rentListings, setRentListings] = useState(null);
+  //now we fill this offerListing using useEffect
+  useEffect(() => {
+    async function fetchListings() {
+      //we use try and catch, in case we have an error
+      try {
+        //for the try we have to get the address reference
+        //we create a const that uses collection method from firestore, that takes the db (from firebase file), and the collection we want to target is "listings"
+        const listingsRef = collection(db, "listings");
+        //create the query which is the condition or the limit of that request
+        //q is using the query method from firestore
+        //then we get the listingsRef, then we want to say where we get the information, so we use "where" from firestore, and the place we want to get is:
+        //where the offer is equal to true(if the offer is true we're going to fetch them)
+        //after that we use the orderBy method from firestore, and we're going to order it by timestamp, in a descending way(the one is created last is going to be the first in the list)
+        const q = query(
+          listingsRef,
+          where("type", "==", "rent"),
+          orderBy("timestamp", "desc"),
+          limit(4) //limiting the listing only of the 4 listings in db
+        );
+        //we need to get the information using snapshot
+        //execute the query
+        //so we create a querySnap which is going to wait for getDocs from firestore
+        const querySnap = await getDocs(q); //and we just put the query inside
+        //this is going to get the information and save it inside a list, then set it inside offerListing from useState
+        const listings = [];
+        //use forEach to loop thorough the querySnap
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+          //now we have everything inside this listings
+        });
+        //we can just set the offer listings to listings
+        setRentListings(listings);
+      } catch (error) {
+        //avoid toasting the error here, because it would be annoying for the users/ we just log it in for us
+        console.log(error);
+      }
+    }
+    fetchListings();
+  }, []);
+  //Places for Sale
+  const [saleListings, setSaleListings] = useState(null);
+  //now we fill this saleListing using useEffect
+  useEffect(() => {
+    async function fetchListings() {
+      //we use try and catch, in case we have an error
+      try {
+        //for the try we have to get the address reference
+        //we create a const that uses collection method from firestore, that takes the db (from firebase file), and the collection we want to target is "listings"
+        const listingsRef = collection(db, "listings");
+        //create the query which is the condition or the limit of that request
+        //q is using the query method from firestore
+        //then we get the listingsRef, then we want to say where we get the information, so we use "where" from firestore, and the place we want to get is:
+        //where the offer is equal to true(if the offer is true we're going to fetch them)
+        //after that we use the orderBy method from firestore, and we're going to order it by timestamp, in a descending way(the one is created last is going to be the first in the list)
+        const q = query(
+          listingsRef,
+          where("type", "==", "sale"),
+          orderBy("timestamp", "desc"),
+          limit(4) //limiting the listing only of the 4 listings in db
+        );
+        //we need to get the information using snapshot
+        //execute the query
+        //so we create a querySnap which is going to wait for getDocs from firestore
+        const querySnap = await getDocs(q); //and we just put the query inside
+        //this is going to get the information and save it inside a list, then set it inside offerListing from useState
+        const listings = [];
+        //use forEach to loop thorough the querySnap
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+          //now we have everything inside this listings
+        });
+        //we can just set the offer listings to listings
+        setSaleListings(listings);
+      } catch (error) {
+        //avoid toasting the error here, because it would be annoying for the users/ we just log it in for us
+        console.log(error);
+      }
+    }
+    fetchListings();
+  }, []);
   return (
     <div>
       <Slider />
+      <div className="max-w-6xl mx-auto pt-4 space-y-6">
+        {/* if offerListings exists and has at least one offer then we want to have a section */}
+        {offerListings && offerListings.length > 0 && (
+          //first we have a title Recent Offers, then a link to the offers page and finally the listings
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Recent Offers</h2>
+            {/* a ling to the offers page */}
+            <Link to="/offers">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more offers
+              </p>
+            </Link>
+            {/* make the ul a grid for the responsiveness */}
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {/* we're going to loop through offerListings */}
+              {offerListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {/* if rentListings exists and has at least one offer then we want to have a section */}
+        {rentListings && rentListings.length > 0 && (
+          //first we have a title places for rent, then a link to the offers page and finally the listings
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">
+              Places for rent
+            </h2>
+            {/* a link to the rent page */}
+            <Link to="/category/rent">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more places for rent
+              </p>
+            </Link>
+            {/* make the ul a grid for the responsiveness */}
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {/* we're going to loop through offerListings */}
+              {rentListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+        {/* if saleListings exists and has at least one offer then we want to have a section */}
+        {saleListings && saleListings.length > 0 && (
+          //first we have a title Sale Offers, then a link to the offers page and finally the listings
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">
+              Places for sale
+            </h2>
+            {/* a ling to the sales page */}
+            <Link to="/category/rent">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more places for sale
+              </p>
+            </Link>
+            {/* make the ul a grid for the responsiveness */}
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {/* we're going to loop through offerListings */}
+              {saleListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,7 +1,175 @@
-import React from 'react'
+import { async } from "@firebase/util";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import ListingItem from "../components/ListingItem";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase";
 
 export default function Offers() {
+  //create a useEffect to fetch the data
+  //put the data in a piece of state that we call listings
+  //make another piece of state for loading because we need to wait until the data is loaded
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
+  useEffect(() => {
+    //async because we use await getDocs from firestore(returns a promise)
+    async function fetchListings() {
+      // use try/catch statement to try to get the info, and if there's an error we toast a message
+      try {
+        //first create the reference
+        //which is the address of that collection (uses the collection from firestore)
+        //the collection takes two things: db- from firebase.js, then we specify the name of the collection "listings"
+        const listingRef = collection(db, "listings");
+        //after the reference, we make the query
+        //creating a variable q which holds the query method from firestore
+        //then query method takes the listing ref we created
+        //then tell it where(from firestore) to get it, which is to "offer" equal true (if the offer is true we want the listings)
+        //we sort it then (orderBy) by time, descending (newest is the first in the list)
+        //the limit it to 8 listings
+        const q = query(
+          listingRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(8)
+        );
+        //we execute the query adding a snapshot called querySnap
+        //which holds the await getDocs(from firestore)
+        const querySnap = await getDocs(q);
+
+        //inside this useEffect, where we're getting the query
+        //we can save the last visible listing
+        //the constant holds the querySnap, and get the doc we want using .docs and inside we tell what document to get, so we use the length of all the docs array minus 1 to get the last item - the index starting from 0
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        //now, after having the last listing from docs, we set it to the last piece of state that we created(and set it to the lastVisible variable)
+        setLastFetchedListing(lastVisible); //now we saved it inside our piece of state, now we can show the button Load More if we have more listings left
+
+        //create the array listings, and loop through querySnap
+        const listings = [];
+        //the loop is going to give us each document, and set each document to the listing by returning
+        //listing.push - which pushes in the array an object which takes the id from doc.id and data from doc.data
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        //we change the value of listings with the setListing state that we set it with the variable listings
+        setListings(listings);
+        //and finally we se the loading to false
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch listing");
+      }
+    }
+    fetchListings();
+  }, []);
+
+  //because we will use more fetched data, we'll make the function async for the button
+  async function onFetchMoreListings() {
+    try {
+      //first create the reference
+      //which is the address of that collection (uses the collection from firestore)
+      //the collection takes two things: db- from firebase.js, then we specify the name of the collection "listings"
+      const listingRef = collection(db, "listings");
+      //after the reference, we make the query
+      //creating a variable q which holds the query method from firestore
+      //then query method takes the listing ref we created
+      //then tell it where(from firestore) to get it, which is to "offer" equal true (if the offer is true we want the listings)
+      //we sort it then (orderBy) by time, descending (newest is the first in the list)
+      //the limit it to 8 listings
+      const q = query(
+        listingRef,
+        where("offer", "==", true),
+        orderBy("timestamp", "desc"),
+        //use the startAfter from firestore, which is going to start from tha lastFetchedListing
+        startAfter(lastFetchedListing),
+        limit(4)
+      );
+      //we execute the query adding a snapshot called querySnap
+      //which holds the await getDocs(from firestore)
+      const querySnap = await getDocs(q);
+
+      //inside this useEffect, where we're getting the query
+      //we can save the last visible listing
+      //the constant holds the querySnap, and get the doc we want using .docs and inside we tell what document to get, so we use the length of all the docs array minus 1 to get the last item - the index starting from 0
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      //now, after having the last listing from docs, we set it to the last piece of state that we created(and set it to the lastVisible variable)
+      setLastFetchedListing(lastVisible); //now we saved it inside our piece of state, now we can show the button Load More if we have more listings left
+
+      //create the array listings, and loop through querySnap
+      const listings = [];
+      //the loop is going to give us each document, and set each document to the listing by returning
+      //listing.push - which pushes in the array an object which takes the id from doc.id and data from doc.data
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      //instead of changing all the listings we'll set the prevState
+      //the we want to return an array, and add the new listings that starts from previous lastFetchedListing
+      setListings((prevState) => [...prevState, ...listings]);
+      //and finally we se the loading to false
+      setLoading(false);
+    } catch (error) {
+      toast.error("Could not fetch listing");
+    }
+  }
+
   return (
-    <div>Offers</div>
-  )
+    <div className="max-w-6xl mx-auto px-3">
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Offers</h1>
+      {/* if the loading is true the show the spinner */}
+      {/* otherwise if the listings exist and there is at least one */}
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        //otherwise in the empty fragment, we're going to loop through the listings that are fetched from the useEffect
+        <>
+          <main>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {/* after the loop we're getting each listing, and we return that
+              listing item component that we import */}
+              {listings.map((listing) => (
+                <ListingItem
+                  //the listingItem has a key that is passed from the listing.id
+                  //an id that passes the listing.id
+                  //finally we pass the data which is equal with listing.data
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </main>
+          {/* adding a load more button, when clicked, we're going to fetch more listings from the database */}
+          {/* need to add a piece of state that tells us if we reached the information of last listing, and we fetch more listings by clicking the button, and start from the last listing forward */}
+          {/* if the last fetched listing exists then we want to show the button  */}
+          {lastFetchedListing && (
+            <div className="flex justify-center items-center">
+              <button
+                onClick={onFetchMoreListings}
+                className="bg-blue-200 px-3 py-1.5 text-gray-700 border border-gray-300 mb-6 mt-6 rounded hover:border-slate-600 transition duration-150 ease-in-out shadow-md"
+              >
+                Load More
+              </button>
+            </div>
+          )}
+        </>
+      ) : (
+        //otherwise put a paragraph that say there aren't offers
+        <p>There are no current offers</p>
+      )}
+    </div>
+  );
 }
